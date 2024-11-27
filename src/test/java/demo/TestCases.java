cc4b86bbad23b7e702871324c723446202b3790b
Arun <arunkandeepan10895@gmail.com>
2024-11-27 14:03:24 +0530

    ME_QA_XYOUTUBE_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -22,20 +22,85 @@ import java.util.logging.Level;
 import demo.utils.ExcelDataProvider;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
+import dev.failsafe.internal.util.Assert;
 
 public class TestCases extends ExcelDataProvider{ // Lets us read the data
-        ChromeDriver driver;
+        public static ChromeDriver driver;
 
-        /*
-         * TODO: Write your tests here with testng @Test annotation.
-         * Follow `testCase01` `testCase02`... format or what is provided in
-         * instructions
-         */
+        @Test
+        public void testCase01() throws InterruptedException{
+        //testCase01: Go to YouTube.com and Assert you are on the correct URL.
+        //Click on "About" at the bottom of the sidebar, and print the message on the screen.
+                boolean status = false;
+                Wrappers test1 = new Wrappers();
+                test1.YoutubeHomePage();
+                status = test1.GetCurrentUrl().contains("youtube.com");
+                org.testng.Assert.assertTrue(status,"Url is Wrong"); 
+                test1.ClickAboutLink();
+                String Text = test1.GetText(By.xpath("//section[@class='ytabout__content']"));
+                System.out.println("Message on About Page : "+Text);
+        } 
 
-        /*
-         * Do not change the provided methods unless necessary, they will help in
-         * automation and assessment
-         */
+        @Test
+        public void testCase02() throws InterruptedException{
+        //testCase02: Go to the "Films" or "Movies" tab and in the 'Top Selling' section, scroll to the extreme right. 
+        //Apply a Soft Assert on whether the movie is marked "A" for Mature or not. 
+        //Apply a Soft assert on the movie category to check if it exists ex: "Comedy", "Animation", "Drama".   
+                boolean status = false;
+                Wrappers test2 = new Wrappers();
+                test2.YoutubeHomePage();  
+                test2.ClickButton("Movies");
+                test2.SectionRightClickExtremeEnd("Top selling");
+                SoftAssert softAssert = new SoftAssert();
+                String MovieMarked = test2.GetSectionLastMovieMarkedAs("Top selling");
+                System.out.println("MovieMarkedAs : "+MovieMarked);
+                status = MovieMarked.equalsIgnoreCase("A");
+                softAssert.assertTrue(status, "Movie not marked AS 'A'");
+                String MovieCategory = test2.GetSectionLastMovieCategory("Top selling");
+                System.out.println("Movie Category : "+MovieCategory);
+                status = MovieCategory.equals("Animation") || MovieCategory.equals("Drama") || MovieCategory.equals("Comedy");
+                softAssert.assertTrue(status, "movie category not exists in 'Comedy', 'Animation', 'Drama'");
+                softAssert.assertAll();
+        }
+
+        @Test
+        public void testCase03() throws InterruptedException{
+        //testCase03: Go to the 'Music' tab and in the 1st section, scroll to the extreme right.
+        //Print the name of the playlist.
+        //Soft Assert on whether the number of tracks listed is less than or equal to 50.
+                boolean status = false;
+                Wrappers test3 = new Wrappers();
+                test3.YoutubeHomePage();
+                test3.ClickButton("Music");
+                test3.SectionRightClickExtremeEnd("This year in music");
+                String NameOfPlaylist = test3.GetSectionLastPlaylistTitle("This year in music");
+                System.out.println("name of the playlist : "+NameOfPlaylist);
+                int NummberOfTracks = test3.GetSectionLastItemNoOfTracks();
+                System.out.println("number of tracks listed : "+NummberOfTracks);
+                status = NummberOfTracks<=50;
+                SoftAssert softAssert = new SoftAssert();
+                softAssert.assertTrue(status);
+        }
+
+        @Test
+        public void testCase04() throws InterruptedException{
+        //testCase04: Go to the 'News' tab and 
+        //print the title and body of the 1st 3 'Latest News Posts' along with the sum of the number of likes on all 3 of them.
+        //No likes given means 0.
+                boolean status = false;
+                Wrappers test4 = new Wrappers();
+                test4.YoutubeHomePage();
+                test4.ClickButton("News");
+                test4.LatestNewsPostTitleAndBody(3);
+                int SumOfLikes = test4.SumOfNumberOfLikes(3);
+                System.out.println("sum of the number of likes on all 3 of them : "+SumOfLikes);
+        }
+
+        @Test
+        public void testCase05(){
+                
+        }
+        
         @BeforeTest
         public void startBrowser() {
                 System.setProperty("java.util.logging.config.file", "logging.properties");
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -6,12 +6,104 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import demo.TestCases;
+
 import java.time.Duration;
+import java.util.List;
 
 public class Wrappers {
-    /*
-     * Write your selenium wrappers here
-     */
+    ChromeDriver driver = TestCases.driver; 
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(50));
+
+
+    public void YoutubeHomePage(){
+        driver.get("https://www.youtube.com/");
+    }
+
+    public String GetCurrentUrl(){
+        return driver.getCurrentUrl();
+    }
+
+    public void ClickAboutLink() throws InterruptedException{
+        WebElement AboutLink = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='About']")));
+        AboutLink.click();
+    }
+
+    public String GetText(By xpath){
+        WebElement Text = wait.until(ExpectedConditions.visibilityOfElementLocated(xpath));
+        return Text.getText();
+    }
+
+    public void ClickButton(String Explore) throws InterruptedException{
+        WebElement Button = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@title='"+Explore+"']")));
+        Button.click();
+        Thread.sleep(6000);
+    }   
+
+
+    public void SectionRightClickExtremeEnd(String SectionName) throws InterruptedException{
+        WebElement RightClick = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='"+SectionName+"']/ancestor::ytd-item-section-renderer//button[@aria-label='Next']")));
+
+        while (RightClick.isDisplayed()) {
+            RightClick.click();
+            Thread.sleep(2000);
+            if(RightClick.isDisplayed()==false){
+                break;
+            }
+        } 
+    }
+
+    public String  GetSectionLastMovieMarkedAs(String SectionName){
+        WebElement MovieMarkedAs = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//span[text()='"+SectionName+"']/ancestor::ytd-item-section-renderer//div[@role=\"img\"][2]/p)[last()]")));
+        return MovieMarkedAs.getText();
+    }
+
+    public String GetSectionLastMovieCategory(String SectionName){
+        WebElement MovieCategory = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//span[text()='"+SectionName+"']/ancestor::ytd-item-section-renderer//h3/following-sibling::span)[last()]")));
+        String[] GetMovieCategory = MovieCategory.getText().trim().split(" ");
+        return GetMovieCategory[0];
+    }
+
+    public String GetSectionLastPlaylistTitle(String SectionName){
+        WebElement GetTitle = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//span[text()='"+SectionName+"']/ancestor::ytd-item-section-renderer//h3//span)[last()]")));
+        return GetTitle.getText();
+    }
+
+
+    public int GetSectionLastItemNoOfTracks(){
+        WebElement GetTitle = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//span[text()='This year in music']/ancestor::ytd-item-section-renderer//div[contains(text(),'songs')])[last()]")));
+        String[] NoOfTracks = GetTitle.getText().split(" ");
+        return  Integer.parseInt(NoOfTracks[0]);
+    }
+
+    public void LatestNewsPostTitleAndBody(int NoOfNews) throws InterruptedException{
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//span[text()='Latest news posts']//ancestor::ytd-rich-section-renderer//a[@id='author-text']//span[@class='style-scope ytd-post-renderer']")));
+        List<WebElement> NewsTitles = driver.findElements(By.xpath("//span[text()='Latest news posts']//ancestor::ytd-rich-section-renderer//a[@id='author-text']//span[@class='style-scope ytd-post-renderer']"));
+
+
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//span[text()='Latest news posts']//ancestor::ytd-rich-section-renderer//*[@id='home-content-text']")));
+        List<WebElement> NewsDescriptions = driver.findElements(By.xpath("//span[text()='Latest news posts']//ancestor::ytd-rich-section-renderer//*[@id='home-content-text']"));
+      
+        for(int i=0;i<NoOfNews;i++){
+            System.out.println("Title : "+NewsTitles.get(i).getText()+" . "+"Description : "+NewsDescriptions.get(i).getText());
+        }
+
+    }
+
+    public int SumOfNumberOfLikes(int NoOfLikes) throws InterruptedException{
+        
+            List<WebElement> Likes = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//span[text()='Latest news posts']//ancestor::ytd-rich-section-renderer//ytd-rich-item-renderer//span[@id='vote-count-middle']")));
+            int count = 0;
+            for(int i=0;i<NoOfLikes;i++){
+                if(Likes.get(i).getText()!=null && Likes.isEmpty()==false){
+                    count = count+(Integer.parseInt(Likes.get(i).getText()));
+                }
+            }
+            return count;
+       
+    }
 }
